6f34b32419c4bd033ccc53e1b476da622174ad0c
Nastya-L <asya.dermenzhi@gmail.com>
2024-01-30 23:52:06 +0200

    [build][BE] Add mongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import type { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 
 const app: Express = express();
 dotenv.config();
-const port = process.env.PORT;
+const {
+  PORT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_PORT
+} = process.env;
+const dbHost = process.env.MONGODB_HOST || 'localhost';
+const mongoString = `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${dbHost}:${DATABASE_PORT}/`;
+
+mongoose.connect(mongoString)
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
