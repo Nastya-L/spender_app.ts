619c05913feaa2baf26df8d3466ce2b2996244c4
Nastya-L <asya.dermenzhi@gmail.com>
2024-07-25 18:19:55 +0300

    [token][BE] Add token expiration check

--- backend/src/middleware/getUserFromToken.ts
+++ backend/src/middleware/getUserFromToken.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from 'express';
 import User from '../models/UserSchema.js';
 import { userMapper } from '../utils/userMapper.js';
 import type { IUserToFE } from '../utils/userMapper.js';
+import isTokenExpired from '../utils/isTokenExpired.js';
 
 export interface IUserRequest extends Request {
   user?: IUserToFE
@@ -11,6 +12,7 @@ const getUserFromToken = (req: IUserRequest, res: Response, next: NextFunction):
   (async () => {
     const bearer = req.headers.authorization?.split(' ');
     const userToken = bearer?.[1];
+    const tokenExpiresInDays: number = Number(process.env.TOKEN_EXPIRES_IN_DAYS) || 1;
 
     if (!userToken) {
       return res.status(401).json({ error: [{ msg: 'Unauthorized user' }] });
@@ -18,6 +20,11 @@ const getUserFromToken = (req: IUserRequest, res: Response, next: NextFunction):
     await User.findOne({ 'token.token': userToken })
       .then(async (userFind) => {
         if (userFind) {
+          const tokenExpired = isTokenExpired(userFind.token, tokenExpiresInDays);
+
+          if (tokenExpired) {
+            return res.status(401).json({ error: [{ msg: 'Token has expired' }] });
+          }
           req.user = userMapper(userFind);
           next();
         } else {
