4f10f5041ad4887868be37db008012129c470bf3
Nastya-L <asya.dermenzhi@gmail.com>
2024-08-23 23:58:28 +0300

    [expense][BE] Add Update expense

--- backend/src/controllers/expenseController.ts
+++ backend/src/controllers/expenseController.ts
@@ -80,6 +80,62 @@ export const createExpense = (req: IUserRequest, res: Response): void => {
   })();
 };
 
+export const updateExpense = (req: IUserRequest, res: Response): void => {
+  (async () => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.jarId) || !mongoose.Types.ObjectId.isValid(req.params.expenseId)) {
+      res.status(400).json({ error: [{ msg: 'ID is not valid' }] });
+      return;
+    }
+
+    const errors = validationResult(req).formatWith(errorFormatter);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ error: errors.array() });
+      return;
+    }
+
+    const jarId: string = req.params.jarId;
+    const expenseId: string = req.params.expenseId;
+    const userId = req.user?._id;
+
+    const expenseValue = req.body.value;
+    const expenseCategory = req.body.category;
+    const expenseDate = req.body.date;
+
+    try {
+      const foundJar = await Jar.findOne({ _id: jarId, users: userId });
+      if (!foundJar) {
+        res.status(404).json({ error: [{ msg: 'No jar found' }] });
+        return;
+      }
+
+      const result = await ExpensePeriod.findOneAndUpdate(
+        { jar: jarId, expenses: { $elemMatch: { _id: expenseId, owner: userId } } },
+        {
+          $set: {
+            'expenses.$.value': expenseValue,
+            'expenses.$.category': expenseCategory,
+            'expenses.$.date': expenseDate
+          }
+        },
+        { new: true }
+      );
+
+      if (!result) {
+        return res.status(503).json({ error: [{ msg: 'Try again later' }] });
+      }
+
+      const updatedExpense = result.expenses.find((exp) => {
+        return String(exp._id) === expenseId;
+      });
+
+      res.status(200).json(updatedExpense);
+    } catch (err) {
+      res.status(500).json({ error: [{ msg: err }] });
+    }
+  })();
+};
+
 export const deleteExpense = (req: IUserRequest, res: Response): void => {
   (async () => {
     if (!mongoose.Types.ObjectId.isValid(req.params.jarId) || !mongoose.Types.ObjectId.isValid(req.params.expenseId)) {
--- backend/src/routes/expenseRoutes.ts
+++ backend/src/routes/expenseRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import getUserFromToken from '../middleware/getUserFromToken.js';
-import { createExpense, deleteExpense, getExpense } from '../controllers/expenseController.js';
+import { createExpense, deleteExpense, getExpense, updateExpense } from '../controllers/expenseController.js';
 import expenseValidator from '../validators/expenseValidator.js';
 
 const expenseRouter = express.Router();
@@ -11,7 +11,7 @@ expenseRouter.get('/jar/:id/expense', getExpense);
 
 expenseRouter.post('/jar/:id/expense', expenseValidator, createExpense);
 
-expenseRouter.put('/:id');
+expenseRouter.put('/jar/:jarId/expense/:expenseId', expenseValidator, updateExpense);
 
 expenseRouter.delete('/jar/:jarId/expense/:expenseId', deleteExpense);
 
